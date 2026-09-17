f3c83fbd9cdfa0a6a788095dbf4ffe400eaef31d
Isaque de Sousa <isaque.error404@gmail.com>
2026-09-17 10:52:28 -0300

    feat: remove joao list

--- src/main/java/org/example/Principal.java
+++ src/main/java/org/example/Principal.java
@@ -20,19 +20,21 @@ public class Principal {
     public static void main(String[] args) {
 
 
-
         List<Funcionario> funcionarios = new ArrayList<>(List.of(
                 new Funcionario("Maria", LocalDate.of(2000, 10, 18), new BigDecimal("2009.44"), "Operador"),
-                new Funcionario("João",    LocalDate.of(1990,  5, 12), new BigDecimal("2284.38"),  "Operador"),
-                new Funcionario("Caio",    LocalDate.of(1961,  5,  2), new BigDecimal("9836.14"),  "Coordenador"),
-                new Funcionario("Miguel",  LocalDate.of(1988, 10, 14), new BigDecimal("19119.88"), "Diretor"),
-                new Funcionario("Alice",   LocalDate.of(1995,  1,  5), new BigDecimal("2234.68"),  "Recepcionista"),
-                new Funcionario("Heitor",  LocalDate.of(1999, 11, 19), new BigDecimal("1582.72"),  "Operador"),
-                new Funcionario("Arthur",  LocalDate.of(1993,  3, 31), new BigDecimal("4071.84"),  "Contador"),
-                new Funcionario("Laura",   LocalDate.of(1994,  7,  8), new BigDecimal("3017.45"),  "Gerente"),
-                new Funcionario("Heloísa", LocalDate.of(2003,  5, 24), new BigDecimal("1606.85"),  "Eletricista"),
-                new Funcionario("Helena",  LocalDate.of(1996,  9,  2), new BigDecimal("2799.93"),  "Gerente")
+                new Funcionario("João", LocalDate.of(1990, 5, 12), new BigDecimal("2284.38"), "Operador"),
+                new Funcionario("Caio", LocalDate.of(1961, 5, 2), new BigDecimal("9836.14"), "Coordenador"),
+                new Funcionario("Miguel", LocalDate.of(1988, 10, 14), new BigDecimal("19119.88"), "Diretor"),
+                new Funcionario("Alice", LocalDate.of(1995, 1, 5), new BigDecimal("2234.68"), "Recepcionista"),
+                new Funcionario("Heitor", LocalDate.of(1999, 11, 19), new BigDecimal("1582.72"), "Operador"),
+                new Funcionario("Arthur", LocalDate.of(1993, 3, 31), new BigDecimal("4071.84"), "Contador"),
+                new Funcionario("Laura", LocalDate.of(1994, 7, 8), new BigDecimal("3017.45"), "Gerente"),
+                new Funcionario("Heloísa", LocalDate.of(2003, 5, 24), new BigDecimal("1606.85"), "Eletricista"),
+                new Funcionario("Helena", LocalDate.of(1996, 9, 2), new BigDecimal("2799.93"), "Gerente")
         ));
+
+
+        funcionarios.removeIf(funcionario -> funcionario.getNome().equals("João"));
 
         System.out.println("=== 3.3 - Lista de funcionários ===");
         for (Funcionario funcionario : funcionarios) {
@@ -43,7 +45,7 @@ public class Principal {
                     funcionario.getFuncao());
         }
 
-        System.out.println("Funcionarios cadstrado: " + funcionarios.size());
+        System.out.println("Funcionarios cadstrados: " + funcionarios.size());
 
     }
 }
